07818319134146bc4324d1f2b6d16f8fa474e074
Jojo <jsramirez@primary.com.ph>
2025-02-27 10:34:29 +0800

    separate property and location fields

--- models/property_list_data.py
+++ models/property_list_data.py
@@ -11,6 +11,18 @@ class PropertyListData(models.Model):
         ('psc', 'PSC'),
         ('ppc', 'PPC')
     ],'Certifying Company')
+
     area = fields.Char('Area')
     remarks = fields.Char('Remarks')
+
+    property_address = fields.Text('Property Address')
+    country = fields.Many2one('res.country', 'Country')
+    state = fields.Many2one('res.country.state', 'State')
+    city = fields.Many2one('res.city', 'City')
+    locality = fields.Many2one('res.locality', 'Locality')
+    pin = fields.Char('Pin')
+    latitude = fields.Char('Latitude')
+    longitude = fields.Char('Longitude')
+
+
     
