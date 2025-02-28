c160eb6a960ddf98fd608931be8de4889972e987
Jojo <jsramirez@primary.com.ph>
2025-02-28 11:41:08 +0800

    manifest data order updated

--- __manifest__.py
+++ __manifest__.py
@@ -7,10 +7,10 @@
     'version': '1.0',
     'depends': ['base'],
     'data': [
-        'security/ir.model.access.csv',
-        'views/menu.xml', 
+        'security/ir.model.access.csv', 
         'views/property_list_data.xml', 
         'views/land_master_data.xml',  
+        'views/menu.xml',
         'data/land_master_xmldata.xml',
         # 'data/remove_land_master_record.xml',
     ],
