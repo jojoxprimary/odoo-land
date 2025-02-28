c922ded835e51ef311915777a8ecb45d3b7656f4
Jojo <jsramirez@primary.com.ph>
2025-02-28 09:02:11 +0800

    commented security api to create master data for land module

--- models/land_master_data.py
+++ models/land_master_data.py
@@ -1,4 +1,4 @@
-from odoo import fields, models, api
+from odoo import fields, models
 
 class LandMasterData(models.Model):
     _name = 'land.master.data'
@@ -8,18 +8,18 @@ class LandMasterData(models.Model):
     description = fields.Char('Description', readonly=True)
     details_ids = fields.One2many('land.master.data.line', 'master_id', string="Details")
 
-    @api.model
-    def create(self, vals):
-        raise ValueError("Creating new records is not allowed for this model.")
+    # @api.model
+    # def create(self, vals):
+    #     raise ValueError("Creating new records is not allowed for this model.")
 
-    def write(self, vals):
-        # Allow updates only for the 'details_ids' field
-        if any(field not in ['details_ids'] for field in vals):
-            raise ValueError("Editing records is not allowed for this model.")
-        return super(LandMasterData, self).write(vals)
+    # def write(self, vals):
+    #     # Allow updates only for the 'details_ids' field
+    #     if any(field not in ['details_ids'] for field in vals):
+    #         raise ValueError("Editing records is not allowed for this model.")
+    #     return super(LandMasterData, self).write(vals)
 
-    def unlink(self):
-        raise ValueError("Deleting records is not allowed for this model.")
+    # def unlink(self):
+    #     raise ValueError("Deleting records is not allowed for this model.")
 
 class LandMasterDataLine(models.Model):
     _name = 'land.master.data.line'
