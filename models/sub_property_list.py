d6faf0fd65646a9ad575af80e5b17c22864406b9
Jojo <jsramirez@primary.com.ph>
2025-03-14 17:50:02 +0800

    add subproperty code and devtype master fields

--- models/sub_property_list.py
+++ models/sub_property_list.py
@@ -10,12 +10,12 @@ class SubPropertyListData(models.Model):
         required=True
     ) # from property list
     name = fields.Char('Sub Property Name', required=True)
-    # subproperty_code = fields.Char('Sub Property Code')
-    # development_type = fields.Many2one(
-    #     'land.master.data.line',
-    #     string='Development Type',
-    #     domain="[('master_id.name', '=', 'Development Type')]"
-    # ) # from development type master 
+    subproperty_code = fields.Char('Sub Property Code')
+    development_type = fields.Many2one(
+        'land.master.data.line',
+        string='Development Type',
+        domain="[('master_id.name', '=', 'Development Type Master')]"
+    ) # from development type master 
 
     #available_area = fields.Float('Available Area')
     #subproperty_land_area = fields.Float('Sub Property Area')
